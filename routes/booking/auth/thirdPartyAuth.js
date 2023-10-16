c8fd660e2c68bff9448b43db9737d003636ebf81
Naveen Dissanayaka <naveen.dissanayake@ebeyonds.com>
2023-10-16 07:48:50 +0530

    google

--- routes/booking/auth/thirdPartyAuth.js
+++ routes/booking/auth/thirdPartyAuth.js
@@ -5,30 +5,35 @@ const admin = require("../../../firebase/firebase");
 
 // Google Sign-up route
 router.post("/signup/google", async (req, res) => {
-  try {
-    const { idToken } = req.body;
-
-    // Verify the Google ID token
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    const { email } = decodedToken;
-
-    // Check if the user already exists in MongoDB
-    let user = await User.findOne({ email });
-
-    if (!user) {
-      // User doesn't exist, create a new user
-      user = new User({
-        email,
-      });
-
-      await user.save();
+    try {
+      const { IdToken } = req.body; // Use the Google ID token from the response
+  
+      if (!IdToken) {
+        return res.status(400).json({ message: "Google ID token is missing in the request." });
+      }
+  
+      // Verify the Google ID token with Firebase
+      const decodedToken = await admin.auth().verifyIdToken(IdToken);
+      const { email } = decodedToken;
+  
+      // Check if the user already exists in MongoDB
+      let user = await User.findOne({ email });
+  
+      if (!user) {
+        // User doesn't exist, create a new user
+        user = new User({
+          email,
+        });
+  
+        await user.save();
+      }
+  
+      res.status(200).json({ message: "Sign-up successful" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Sign-up failed" });
     }
-
-    res.status(200).json({ message: "Sign-up successful" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Sign-up failed" });
-  }
-});
+  });
+  
 
 module.exports = router;
