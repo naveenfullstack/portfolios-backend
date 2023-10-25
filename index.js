5e27e25d493146f42138ddf3cf70d06c05a0adc0
Naveen Dissanayaka <naveen.dissanayake@ebeyonds.com>
2023-10-25 07:40:16 +0530

    webhook

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
-// Import packages
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
-//Database
-
-const connectToDatabase = require('./databases/mainmongodb');
-connectToDatabase();
-
-// Middlewares
+const cors = require("cors");
+const http = require("http");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const socketIo = require("socket.io"); // Add this line to import socket.io
+require("dotenv").config();
+const server = http.createServer(app); // Create an HTTP server using Express app
+const io = socketIo(server, {
+  cors: {
+    origin: ["http://localhost:3000","http://192.168.1.8:3000"],
+    methods: ["GET", "POST"],
+  },
+}); // Initialize socket.io on the server
 
-const Headers = require ('./middlewares/header')
+// Database
+const connectToDatabase = require("./databases/mainmongodb");
+connectToDatabase();
 
-const ReqDomain = require ('./middlewares/reqdomain')
+// Middlewares
+
+const Headers = require("./middlewares/header");
+
+const ReqDomain = require("./middlewares/reqdomain");
 app.use(ReqDomain);
 
 //Routes
 
 // Add redirection to naveenportfolio.site
-app.get('/', (req, res) => {
-    res.redirect('https://naveenportfolio.site');
-  });
+app.get("/", (req, res) => {
+  res.redirect("https://naveenportfolio.site");
+});
 
-const TestData = require('./routes/testdata');
-app.use('/', TestData);
+const TestData = require("./routes/testdata");
+app.use("/", TestData);
 
-const EMS = require('./routes/ems/index');
-app.use('/ems', EMS) , Headers;
+const EMS = require("./routes/ems/index");
+app.use("/ems", EMS), Headers;
 
-const MediaX = require('./routes/mediax/index');
-app.use('/mediax', MediaX) , Headers;
+const MediaX = require("./routes/mediax/index");
+app.use("/mediax", MediaX), Headers;
 
-const Portfolio = require('./routes/portfolio/index');
-app.use('/portfolio', Portfolio) , Headers;
+const Portfolio = require("./routes/portfolio/index");
+app.use("/portfolio", Portfolio), Headers;
 
-const Booking = require('./routes/booking/index');
-app.use('/booking', Booking) , Headers;
+const Booking = require("./routes/booking/index");
+app.use("/booking", Booking), Headers;
 
-const Finance = require('./routes/finance/index');
-app.use('/finance', Finance) , Headers;
+const Finance = require("./routes/finance/index");
+app.use("/finance", Finance), Headers;
 
 // connection
 const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+server.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
+
+// Now, you can use io to handle socket.io events.
+io.on("connection", (socket) => {
+  console.log(`A user connected to socket.io ${socket.id}`);
+  // Handle socket.io events here
+});
--- middlewares/reqdomain.js
+++ middlewares/reqdomain.js
@@ -1,19 +1,23 @@
 const ReqDomain = (req, res, next) => {
-    // Extract the domain from the 'referer' header of the request
-    const referer = req.get('referer');
-
-    // Parse the referer URL to get the domain
-    const domain = referer ? new URL(referer).hostname : 'Unknown Domain';
-
-    // Get the current date and time
-    const now = new Date();
-    const formattedDate = `${now.getFullYear()} ${now.toLocaleString('default', { month: 'short' })} ${now.getDate()} at ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${now.getHours() >= 12 ? 'PM' : 'AM'}`;
-
-    // Log the domain, request time and date, and client's IP address to the console
-    console.log(`${domain} has requested at ${formattedDate}`);
-
-    // Continue processing the request
-    next();
+  // Define ANSI escape codes for green text color
+  const greenColor = "\x1b[32m";
+  const resetColor = "\x1b[0m";
+  // Extract the domain from the 'referer' header of the request
+  const referer = req.get("referer");
+  // Parse the referer URL to get the domain
+  const domain = referer ? new URL(referer).hostname : "Unknown Domain";
+  // Get the current date and time
+  const now = new Date();
+  const formattedDate = `${now.getFullYear()} ${now.toLocaleString("default", {
+    month: "short",
+  })} ${now.getDate()} at ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${
+    now.getHours() >= 12 ? "PM" : "AM"
+  }`;
+  // Log the domain, request time, and requested URL to the console
+  console.log(
+    `${greenColor}${domain} ${resetColor}has requested at ${formattedDate} URL: ${greenColor}${req.originalUrl}`
+  );
+  // Continue processing the request
+  next();
 };
-
 module.exports = ReqDomain;
