1e77c3d76f567656761f770246f9ecf3a86c4bd1
Naveen Dissanayaka <naveen.dissanayake@ebeyonds.com>
2023-10-26 08:01:51 +0530

    b

--- routes/finance/auth/balance.js
+++ routes/finance/auth/balance.js
@@ -2,43 +2,33 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../../../models/finance/user'); // Update the path to your User model
-const app = require('../../../index'); // Update the path accordingly
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io')(server);
 
-// GET request to fetch user balance by user ID from query parameter
-router.get('/view', async (req, res) => {
+// GET request to fetch user balance by user ID
+router.get('/view/:userId', async (req, res) => {
   try {
-    const userId = req.query.userId;
-    
-    if (!userId) {
-      return res.status(400).json({ message: 'Missing userId query parameter' });
-    }
-    
+    const userId = req.params.userId;
+
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       return res.status(400).json({ message: 'Invalid user ID' });
     }
-    
+
     const user = await User.findById(userId).select('balance');
-    
+
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-    
-    // Emit the balance update to connected clients
-    io.sockets.emit('balanceUpdate', { userId, balance: user.balance });
-    
-    res.status(200).json({
-      success: true,
-      message: 'Login Success',
-      balance: user.balance,
-    });
+
+    res
+      .status(200)
+      .json({
+        success: "true",
+        message: "Login Success",
+        balance : user.balance,
+      });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server Error' });
   }
 });
-
 
 module.exports = router;
